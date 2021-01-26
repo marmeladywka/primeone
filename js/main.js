0d6304db118601ef187e56574ab9e7d7270d1cc1
marmeladywka <74825525+marmeladywka@users.noreply.github.com>
2021-01-26 17:16:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,19 +7,3 @@ let isMobile = {
 	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
 };
 
-let body=document.querySelector('body');
-
-if(isMobile.any()){
-	body.classList.add('touch');
-	let header__mobile-link=document.querySelectorAll('.arrow');
-	for(i=0; i<header__mobile-link.length; i++){
-	let header__mobile-list=header__mobile-link[i].nextElementSibling;
-	let thisArrow=header__mobile-link[i];
-	header__mobile-link[i].addEventListener('click', function(){
-			header__mobile-list.classList.toggle('open');
-			header__mobile-link.classList.toggle('active');
-		});
-	}	
-	}else{
-		body.classList.add('mouse');
-	}
